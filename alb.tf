17f5e0fad6244bc5194d8fe3c8e7277b1d965709
Arun Sunuwar <arunsunuwar@aruns-mbp.lan>
2020-03-03 20:42:59 -0500

    change

--- alb.tf
+++ alb.tf
@@ -3,13 +3,12 @@ resource "aws_lb" "default" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = ["${aws_security_group.default_lb.id}"]
-  subnets            = ["subnet-0a59446d", "subnet-3af0e814"]
+  subnets            = ["subnet-21eec31f", "subnet-69dd3948"]
   enable_deletion_protection = false
   tags = {
     Environment = "dev"
   }
 }
-
 resource "aws_lb_listener" "default" {
   load_balancer_arn = "${aws_lb.default.arn}"
   port              = "80"
@@ -19,11 +18,9 @@ resource "aws_lb_listener" "default" {
     target_group_arn = "${aws_lb_target_group.default.arn}"
   }
 }
-
 resource "aws_lb_target_group" "default" {
   name     = "terraform-ecs-bulbapp"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "vpc-7490be0e"
+  vpc_id   = "vpc-f8a48e82"
 }
-
